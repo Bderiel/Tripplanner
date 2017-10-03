d43f6710d25eabc41b5caf555dc6373a9e60ca7a
Jonathan Chang <jonathanmchang@gmail.com>
2017-10-03 18:31:54 -0400

    about to seed

--- models/index.js
+++ models/index.js
@@ -1,9 +1,26 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/tripDB', {logging: false})
 
+const Place = db.define('place', {
+    address:{
+        type: Sequelize.STRING
+    },
+    city: {
+        type: Sequelize.STRING
+    },
+    state: {
+        type: Sequelize.STRING
+    },
+    phone: {
+        type: Sequelize.STRING
+    },
+    location: {
+        type: Sequelize.ARRAY(Sequelize.FLOAT,Sequelize.FLOAT)
+    }
+})
+
 const Hotel = db.define('hotel',{
     name:{type:Sequelize.STRING},
-    place:{type:Sequelize.JSON},
     num_stars:{type:Sequelize.FLOAT},
     amenities:{type:Sequelize.TEXT}
     
@@ -11,15 +28,18 @@ const Hotel = db.define('hotel',{
 
 const Restaurant = db.define('restaurant',{
     name:{type:Sequelize.STRING},
-    place:{type:Sequelize.JSON},
     cuisine:{type:Sequelize.STRING},
     price: {type:Sequelize.INTEGER}
 })
 
 const Activity = db.define('activity',{
     name: {type:Sequelize.STRING},
-    place: {type:Sequelize.JSON},
     age_range: {type:Sequelize.ENUM('ALL','21+')}
 })
+
+Hotel.belongsTo(Place);
+Restaurant.belongsTo(Place);
+Activity.belongsTo(Place);
+
 
 module.exports = {Hotel, Restaurant, Activity, db}
